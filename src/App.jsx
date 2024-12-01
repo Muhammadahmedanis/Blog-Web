36da206994656b7a161158c5523d82ab9a836f3e
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2024-12-01 11:05:06 +0500

    addenvfile

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return(
+    <div className='App'>
+      <h1>blo</h1>
+    </div>
+  )
+}
+
+export default App
