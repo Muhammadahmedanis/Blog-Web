e62ed0883d2ebc6b664ea45c2cf30461a7c5cc71
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2024-12-03 17:47:18 +0500

    working on component and appwrite

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blogweb
- 
+Slug: the part of a website URL that identifies a specific page on the site
+forwardref: use to pass data from parent component to child component
---
+++ src/appwrite/configuration.js
@@ -0,0 +1,124 @@
+import config from "../config/config";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+                .setEndpoint(config.appwriteUrl)
+                .setProject(config.appwriteProjectId);
+                this.databases = new Databases(this.client);
+                this.bucket = new Storage(this.client);
+    }
+
+    async createPost({ title, slug, content, featturedImage, status, userId }){
+        try {
+            return await this.databases.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, { title, content, featturedImage, status, userId  })
+        } catch (error) {
+            console.log("error in create post");
+            throw error
+        }
+    }
+
+    async updatePost(slug, { title, content, featturedImage, status }){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featturedImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            console.log("error in update post");
+            throw error;
+        }
+    }
+
+
+    async deletePost(slug){
+        try {
+                await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+            return true;
+        } catch (error) {
+            console.log("error in delete post");
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("error in get post");
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query .equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+                // [ Query .equal("status", "active") ]
+            )
+        } catch (error) {
+            console.log("error in get all post");
+            return false;
+        }
+    }
+
+    // file upload services
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("error inupload file");
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+                await this.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("error");
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId,
+        );
+    }
+
+};
+
+const service = new Service();
+export default service;
